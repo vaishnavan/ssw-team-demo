c704dd409f781ff0081b19f35dddd127020165c9
vaishnavanm5 <vaishnavanm5@gmail.com>
2023-04-15 10:26:48 +0530

    feat: adding some example with counter

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,41 @@ import React from 'react'
 import './Home.css'
 
 class Home extends React.PureComponent {
+  constructor() {
+    super()
+    this.state = {
+      count: 0,
+    }
+    this.handleIncrement = this.handleIncrement.bind(this)
+    this.handleDecrement = this.handleDecrement.bind(this)
+  }
+
+  handleIncrement() {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }))
+  }
+
+  handleDecrement() {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }))
+  }
+
   render() {
+    const { count } = this.state
     return (
       <div className="Home">
-        <h1>helo vaishnavan</h1>
+        <h2>
+          Count value is
+          {count}
+        </h2>
+        <button onClick={this.handleIncrement} type="button">
+          Increment
+        </button>
+        <button onClick={this.handleDecrement} type="button">
+          Decrement
+        </button>
       </div>
     )
   }
